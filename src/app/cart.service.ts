2bc813ed31cc2acc48fa0390a78f1c4ce98a2541
Samuelrovoli <128751847+Samuelrovoli@users.noreply.github.com>
2023-06-20 18:19:45 -0300

    Initial commit

---
+++ src/app/cart.service.ts
@@ -0,0 +1,27 @@
+
+import { Product } from './products';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+  /*propriedade para armazenar a matriz dos produtos atuais no carrinho.*/
+
+  items: Product[] = [];
+ 
+  addToCart(product: Product) {
+    this.items.push(product);
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+
+}
+
